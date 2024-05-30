27a3bf1c55fb354971e638d2de52d12c45d17af8
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 08:15:34 +0100

    Add Dtos (User, Bug)

---
+++ SimpleBugTracker.Application/Bugs/Dto/BugDto.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using SimpleBugTracker.Application.Users.Dto;
+using SimpleBugTracker.Domain.Entities;
+using SimpleBugTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBugTracker.Application.Bugs.Dto
+{
+    public class BugDto
+    {
+        public int BugId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public PriorityLevel Priority { get; set; } = PriorityLevel.Low;
+
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateClosed { get; set; }
+
+
+        public UserDto AssignedUser { get; set; } = new UserDto();
+
+        private class Mapping : Profile
+        {
+            public Mapping()
+            {
+                CreateMap<Bug, BugDto>().ReverseMap();
+            }
+        }
+    }
+}
---
+++ SimpleBugTracker.Application/Users/Dto/UserDto.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using SimpleBugTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBugTracker.Application.Users.Dto
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public class Mapping : Profile
+        {
+            public Mapping()
+            {
+                CreateMap<User, UserDto>().ReverseMap();
+            }
+        }
+    }
+}
