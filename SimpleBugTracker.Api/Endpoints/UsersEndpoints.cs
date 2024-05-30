b1d68f9dd0ef904358c7df94e88262665c88087e
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 08:56:20 +0100

    Add UpdateUser endpoint

--- SimpleBugTracker.Api/Endpoints/UsersEndpoints.cs
+++ SimpleBugTracker.Api/Endpoints/UsersEndpoints.cs
@@ -13,6 +13,7 @@ namespace SimpleBugTracker.API.Endpoints
 
             group.MapGet("", GetUsers);
             group.MapPost("", CreateUser);
+            group.MapPut("", UpdateUser);
         }
 
         public static async Task<IResult> GetUsers(ISender sender, [AsParameters] GetUsersQuery query)
@@ -30,6 +31,20 @@ namespace SimpleBugTracker.API.Endpoints
         }
 
         public static async Task<IResult> CreateUser(ISender sender, [FromBody] CreateUserCommand command)
+        {
+            try
+            {
+                var ret = await sender.Send(command);
+
+                return Results.Ok(ret);
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        }
+
+        public static async Task<IResult> UpdateUser(ISender sender, [FromBody] UpdateUserCommand command)
         {
             try
             {
---
+++ SimpleBugTracker.Application/Users/Commands/UpdateUser.cs
@@ -0,0 +1,43 @@
+﻿using SimpleBugTracker.Application.Data;
+using SimpleBugTracker.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using SimpleBugTracker.Application.Users.Dto;
+
+namespace SimpleBugTracker.Application.Users.Commands
+{
+    public record UpdateUserCommand : IRequest<int>
+    {
+        public UserDto? User { get; set; }
+    }
+
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateUserCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var userId = request.User.UserId;
+            var user = await _context.Users.FindAsync(userId, cancellationToken);
+
+            Guard.Against.NotFound(userId, user);
+
+            if (user.Name != request.User.Name) { user.Name = request.User.Name; }
+            if (user.Email != request.User.Email) { user.Email = request.User.Email; }
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return user.UserId;
+        }
+    }
+}
