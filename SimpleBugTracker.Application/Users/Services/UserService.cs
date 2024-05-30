3f92a7e96e0e1bae169d403dde7cbc6a89f25c2d
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 09:59:10 +0100

    Add UserService

---
+++ SimpleBugTracker.Application/Users/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+using SimpleBugTracker.Application.Data;
+using SimpleBugTracker.Application.Users.Dto;
+using SimpleBugTracker.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleBugTracker.Application.Users.Services
+{
+    public interface IUserService
+    {
+        Task<User> GetUserFromDto(IApplicationDbContext context, CancellationToken cancellationToken, UserDto dto);
+    }
+
+    public class UserService : IUserService
+    {
+        async public Task<User> GetUserFromDto(IApplicationDbContext context, CancellationToken cancellationToken, UserDto dto)
+        {
+            var assignedUserId = dto.UserId;
+            var assignedUser = await context.Users.FindAsync(assignedUserId, cancellationToken);
+            Guard.Against.NotFound(assignedUserId, assignedUser);
+            return assignedUser;
+        }
+    }
+}
