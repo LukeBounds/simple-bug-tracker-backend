e4e10912618a9337c6eb76d939da90709c9e1530
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 07:55:00 +0100

    Add Domain entities

---
+++ SimpleBugTracker.Domain/Entities/Bug.cs
@@ -0,0 +1,23 @@
+﻿using SimpleBugTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBugTracker.Domain.Entities
+{
+    public class Bug
+    {
+        public int BugId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public PriorityLevel Priority { get; set; } = PriorityLevel.Low;
+
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateClosed { get; set; }
+
+        public virtual User AssignedUser { get; set; } = new User();
+    }
+}
---
+++ SimpleBugTracker.Domain/Enums/PriorityLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBugTracker.Domain.Enums
+{
+    public enum PriorityLevel
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2,
+    }
+}
