26709e06374e21661c8625da01edbc877c30f64c
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 08:40:51 +0100

    Add CreateUserTest

---
+++ SimpleBugTracker.Application.Test/Users/Commands/CreateUserTest.cs
@@ -0,0 +1,38 @@
+﻿using SimpleBugTracker.Application.Users.Commands;
+using SimpleBugTracker.Application.Users.Dto;
+
+namespace SimpleBugTracker.Application.Test.Users.Commands
+{
+    internal class CreateUserTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task CreateUserTest1()
+        {
+            var db = Testing.GetTestContext();
+
+            var userDto = new UserDto();
+
+            var command = new CreateUserCommand()
+            {
+                User = userDto,
+            };
+
+            var handler = new CreateUserCommandHandler(db, Testing._mapper);
+            //--
+
+            var userId = await handler.Handle(command, CancellationToken.None);
+
+            var addedUser = db.Users.Find(userId);
+            //--
+
+            Assert.True(userId != 0);
+            Assert.IsNotNull(addedUser);
+        }
+    }
+}
