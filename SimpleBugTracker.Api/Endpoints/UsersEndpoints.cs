f428f5e10af39b9a235f4dbb4caef25c278496e6
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 08:30:57 +0100

    Add GetUsers endpoint

---
+++ SimpleBugTracker.Api/Endpoints/UsersEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using SimpleBugTracker.Application.Users.Queries;
+using MediatR;
+
+namespace SimpleBugTracker.API.Endpoints
+{
+    public static class UsersEndpoints
+    {
+        public static void MapUsersEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/users");
+
+            group.MapGet("", GetUsers);
+        }
+
+        public static async Task<IResult> GetUsers(ISender sender, [AsParameters] GetUsersQuery query)
+        {
+            try
+            {
+                var ret = await sender.Send(query);
+
+                return Results.Ok(ret);
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ SimpleBugTracker.Application/Users/Queries/GetUsers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
+using AutoMapper;
+using SimpleBugTracker.Application.Data;
+using SimpleBugTracker.Application.Users.Dto;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleBugTracker.Application.Users.Queries
+{
+    public record GetUsersQuery : IRequest<ICollection<UserDto>>
+    {
+
+    }
+
+    public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, ICollection<UserDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetUsersQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ICollection<UserDto>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _context.Users.ProjectTo<UserDto>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+
+            return users;
+        }
+    }
+}
