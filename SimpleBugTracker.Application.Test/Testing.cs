cca6219cd8b0d6e42a196b0d774ff3f8567a0363
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 08:38:01 +0100

    Add Testing (setup and helpers)

---
+++ SimpleBugTracker.Application.Test/Testing.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using SimpleBugTracker.Application.Data;
+using SimpleBugTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace SimpleBugTracker.Application.Test
+{
+    [SetUpFixture]
+    internal class Testing
+    {
+
+        public static IMapper _mapper;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var configuration = new MapperConfiguration(config =>
+                config.AddMaps(Assembly.GetAssembly(typeof(IApplicationDbContext))));
+            _mapper = configuration.CreateMapper();
+        }
+
+        public static IApplicationDbContext GetTestContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+            .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
